f81ce0980341d292600cd2ed37195473f1642dbe
bradygaster <bradyg@microsoft.com>
2021-05-07 10:25:55 -0700

    added onconnected

--- src/chat.web/Hubs/ChatHub.cs
+++ src/chat.web/Hubs/ChatHub.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
 using Microsoft.Identity.Web.Resource;
 
 namespace chat.web.Hubs
@@ -10,12 +11,26 @@ namespace chat.web.Hubs
     [RequiredScope("Chat")]
     public class ChatHub : Hub
     {
+        private readonly ILogger<ChatHub> logger;
+
+        public ChatHub(ILogger<ChatHub> logger)
+        {
+            this.logger = logger;
+        }
+        
         public async Task SendMessage(string message)
         {
             await Clients.All.SendAsync("messageReceived", new {
                 text = message,
                 username = this.Context.User.Identity.Name
             });
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            var username = this.Context.User.Identity.Name;
+            logger.LogInformation($"{username} just logged in and connected");
+            return Task.CompletedTask;
         }
     }
 }
