31cc9cbcef024b3061a35e380126ca4fb8ac0411
bradygaster <bradyg@microsoft.com>
2021-05-08 23:37:10 -0700

    options class

---
+++ src/chat/DotNetChatOptions.cs
@@ -0,0 +1,9 @@
+namespace chat
+{
+    public class DotNetChatOptions
+    {
+        public string RedirectUrl { get; set; }
+        public string ClientId { get; set; }
+        public string ChatScope { get; set; }
+    }
+}
