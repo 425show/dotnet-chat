1d8001f17c919c339ab9bfad04b061f18fbc4be5
bradygaster <bradyg@microsoft.com>
2021-05-08 23:37:22 -0700

    wire up configuration

--- src/chat/Program.cs
+++ src/chat/Program.cs
@@ -2,6 +2,11 @@
 using System.Linq;
 using System.Threading.Tasks;
 using chat.Commands;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.Identity.Client;
 
 namespace chat
@@ -9,33 +14,42 @@ namespace chat
     class Program
     {
         static string _accessToken = string.Empty;
-        static string _redirectUri = "http://localhost";
-        static string _clientId = "";
-        static string _scope = "";
-
+        static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args);
         static async Task Main(string[] args)
         {
+            DotNetChatOptions options = new();
+
+            CreateHostBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, configuration) =>
+                {
+                    options = configuration
+                                .Build()
+                                    .GetSection(nameof(DotNetChatOptions))
+                                    .Get<DotNetChatOptions>();
+                })
+                .Build();
+
             IPublicClientApplication app = PublicClientApplicationBuilder
-                .Create(_clientId)
-                .WithRedirectUri(_redirectUri)
+                .Create(options.ClientId)
+                .WithRedirectUri(options.RedirectUrl)
                 .WithAuthority(AadAuthorityAudience.AzureAdMultipleOrgs)
                 .Build();
 
             AuthenticationResult result;
             var accounts = await app.GetAccountsAsync();
-            var scopes = new string[] { _scope };
+            var scopes = new string[] { options.ChatScope };
 
             try
-            { 
-                result = await app.AcquireTokenSilent(scopes, 
-                    accounts.FirstOrDefault()).ExecuteAsync(); 
+            {
+                result = await app.AcquireTokenSilent(scopes,
+                    accounts.FirstOrDefault()).ExecuteAsync();
             }
-            catch (MsalUiRequiredException) 
-            { 
-                result = await app.AcquireTokenInteractive(scopes).ExecuteAsync(); 
+            catch (MsalUiRequiredException)
+            {
+                result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
             }
 
-            if(result != null)
+            if (result != null)
             {
                 _accessToken = result.AccessToken;
             }
@@ -48,7 +62,6 @@ namespace chat
                 HandleInput(input);
             }
         }
-
 
         static void HandleInput(string input)
         {
